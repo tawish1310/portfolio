2f792706c592783bf9b20cab9212e007336086db
Tawish Naqvi <tanaqvi@microsoft.com>
2021-01-26 09:59:53 -0800

    Adding First Initial Files

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Layout from "../components/layout"
+
+export default function About() {
+  return (
+    <div>
+    <Layout></Layout>
+      <h1>About me</h1>
+      <p>
+        I am a Full Stack Software Engineer who loves to go by the codename "Hijabi Developer"
+        I have been working in the tech industry for the last decade and have seen it all.
+
+      </p>
+    </div>
+  )
+}
---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../components/layout";
+
+export default function Contact() {
+  return (
+    <div>
+        <Layout></Layout>
+      <h1>Containers</h1>
+      <p>
+        <a href="mailto:me@example.com">me@example.com</a>
+      </p>
+    </div>
+  )
+}
