f13795e3d29fa3279fcafcc42429332385246e11
Tawish Naqvi <tanaqvi@microsoft.com>
2021-03-26 23:51:15 -0700

    First Publish

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,7 +4,7 @@ import Footer from "../pages/Footer"
 import logo from "../styles/logo1.jpg"
 import Grid from "@material-ui/core/Grid"
 import { makeStyles } from "@material-ui/core/styles"
-
+import { SocialIcon } from "react-social-icons"
 
 const ListLink = props => (
   <li
@@ -12,18 +12,17 @@ const ListLink = props => (
       display: `inline`,
       marginRight: `1rem`,
       marginTop: `33px`,
-      float:`right`
+      float: `right`,
     }}
   >
     <Link
       href={props.to}
-      style={{ color: "black", backgroundImage: "none"}}
+      style={{ color: "black", backgroundImage: "none" }}
       underline="always"
       aria-label={props.children}
     >
       {props.children}
     </Link>
-    
   </li>
 )
 
@@ -32,10 +31,49 @@ export default function Layout({ children }) {
   return (
     <div className="header">
       <Grid>
-        <img src={logo} className={classes.img} alt="name Logo" />
         <ListLink to="/work/">Work</ListLink>
         <ListLink to="/">About</ListLink>
-       
+        <img src={logo} className={classes.img} alt="name Logo" />
+        <br />
+
+        <SocialIcon
+          url="https://www.linkedin.com/in/tawish/"
+          bgColor="#000000"
+          style={{
+            height: 25,
+            width: 25,
+            textShadow: `none`,
+            backgroundImage: `none`,
+            left: `35px`,
+          }}
+        />
+        <span style={{ paddingLeft: `40px` }}>&bull;</span>
+
+        <SocialIcon
+          url="https://github.com/tawish1310"
+          bgColor="#000000"
+          style={{
+            height: 25,
+            width: 25,
+            textShadow: `none`,
+            backgroundImage: `none`,
+            left: `10px`,
+          }}
+        />
+        <span style={{ paddingLeft: `15px` }}>&bull;</span>
+
+        <SocialIcon
+          url="https://medium.com/@tawish"
+          bgColor="#000000"
+          style={{
+            height: 25,
+            width: 25,
+            textShadow: `none`,
+            backgroundImage: `none`,
+            left: `10px`,
+          }}
+        />
+        
         {children}
       </Grid>
     </div>
